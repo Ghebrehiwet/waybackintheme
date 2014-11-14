1c5a18da3899a352e56bd1bdce41cc7db4e8d79a
Belvatron <r4gn4rkv3rn@gmail.com>
2014-11-13 19:27:36 -0800

    Reworked the image

--- header.php
+++ header.php
@@ -2,10 +2,6 @@
 <html>
 <head <?php language_attributes(); ?>>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<h1><a href="index.php"> <!--<span><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?>
-</a></span> <!-- for SEO -->
-     <img src="<?php bloginfo('template_url'); ?>/images/fakelogo.jpg" id="logo"  
-       alt="My Logo"> <!-- for screen readers --></a></h1>
 	<title><?php bloginfo("name") . wp_title('|'); ?></title>	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">	
 	<!--[if lt IE 9]>
@@ -15,11 +11,10 @@
 </head>
 <body <?php body_class($class); ?>>
 	<header id="main_header">
-		<hgroup>
-			<h1><a href="<?php echo get_option('home');?>/"<?php bloginfo("name"); ?></h1>
-			<h2><?php bloginfo("description"); ?></h2>
-		</hgroup>
-		
+			<h1><a href="index.php"> <!--<span><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?>
+			</a></span> <!-- for SEO -->
+			<img src="<?php bloginfo('template_url'); ?>/images/fakelogo.jpg" id="logo"  
+			alt="My Logo"> <!-- for screen readers --></a></h1>		
 		<nav>
 			<?php wp_nav_menu(array('menu' => 'Primary Menu')); ?>
 		</nav>
