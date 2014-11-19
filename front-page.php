22195179d488dc56658f078e93c67ecaf0f3d493
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-18 23:26:31 -0800

    adds front-page.php

---
+++ front-page.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Template Name: Front Page
+*/
+?>
+<?php get_header(); ?>
+
+<div id="main-content">
+			<div id="welcome">
+            	<div id="welcomebox">
+					<span>Welcome to Way Back Inn</span>
+                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi 
+                </div>
+			</div>
+			<div id="flexslider">
+				Flexslider
+
+			</div>
+			<div id="button-container1">
+				<ul id="button-list1">
+					<li id="button-one"><a href="#"><p>Item one</p></a></li>
+					<li id="button-two"><a href="#"><p>Item two</p></a></li>
+					<li id="button-three"><a href="#"><p>Item three</p></a></li>
+				</ul>
+			</div>
+
+		<div id="button-container2">
+			<ul id="button-list2">
+				<li id="button-four"><a href="#"><p>Item four</p></a></li>
+				<li id="button-five"><a href="#"><p>Item five</p></a></li>
+				<li id="button-six">
+					<div id="social-networking">
+						<div id="find-us"><p><strong>Find Us:</strong></p></div>
+						<div id="facebook"></div>
+						<div id="twitter"></div>
+					</div>
+				</li>
+
+			</ul>
+		</div>
+			<div id="testimonials">
+				Testimonials
+			</div>
+			<div id="ads">
+				Ads
+			</div> 
+<?php get_footer(); ?>
+
