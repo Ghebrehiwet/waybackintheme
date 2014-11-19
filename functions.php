66d0691de62faf8f0f7889d9c1fd478a1851caa1
Ghebre Ghebremedhin <geberhane@gmail.com>
2014-11-18 20:21:50 -0800

    Fix cookie error by not writing PHP that crashes

--- functions.php
+++ functions.php
@@ -1,40 +1,33 @@
-<?php register_nav_menus(); ?>
+<?php 
 
-<?php register_nav_menu("main_menu", "Main Menu"); ?>
+register_nav_menus();
 
-<!--<?php wp_nav_menu($mainMenu); ?>-->
+register_nav_menu("main_menu", "Main Menu");
 
-<?php add_theme_support("post-thumbnails"); ?>
+//wp_nav_menu($mainMenu);
 
-<?php set_post_thumbnail_size(200,200, true); ?>
+add_theme_support("post-thumbnails");
 
-<?php add_post_type_support("page", "excerpt"); ?>
+set_post_thumbnail_size(200,200, true); ?>
 
-<?php the_post_thumbnail(); ?>
+add_post_type_support("page", "excerpt"); ?>
 
+//menu
+$mainMenu = array(
+	"theme_location" => "main_menu",
+	"container" => "ul",
+	"container_class" => "",
+	"container_id" => "main_menu",
+	"depth" => 1
+);
 
-
-
-<?php
-	//menu
-	$mainMenu = array(
-		"theme_location" => "main_menu",
-		"container" => "ul",
-		"container_class" => "",
-		"container_id" => "main_menu",
-		"depth" => 1
-	);
-?>
-
-<?php
-	//sidebars
-	register_sidebar(array(
-		"name" => "Right Side Bar",
-		"id" => "right_aside",
-		"description" => "Right Side Bar",
-		"before_widget" => "<div class='widget'>",
-		"after_widget" => "</div><!-- end widget -->",
-		"before_title" => "<h3 class='widget_title'>",
-		"after_title" => "</h3>"
-	));
-?>
+//sidebars
+register_sidebar(array(
+	"name" => "Right Side Bar",
+	"id" => "right_aside",
+	"description" => "Right Side Bar",
+	"before_widget" => "<div class='widget'>",
+	"after_widget" => "</div><!-- end widget -->",
+	"before_title" => "<h3 class='widget_title'>",
+	"after_title" => "</h3>"
+));
