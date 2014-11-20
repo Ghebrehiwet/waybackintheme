3cdb6324be43b4acc264b63c036a2aa6ffc2891b
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-19 22:37:53 -0800

    adds HTML and CSS for front-page.php

--- front-page.php
+++ front-page.php
@@ -7,10 +7,11 @@ Template Name: Front Page
 
 <div id="main-content">
 			<div id="welcome">
-            	<div id="welcomebox">
-					<span>Welcome to Way Back Inn</span>
-                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi 
-                </div>
+
+
+					<p>Welcome to Way Back Inn Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.
+					</p>
+
 			</div>
 			<div id="flexslider">
 				Flexslider
@@ -18,28 +19,31 @@ Template Name: Front Page
 			</div>
 			<div id="button-container1">
 				<ul id="button-list1">
-					<li id="button-one"><a href="#"><p>Item one</p></a></li>
-					<li id="button-two"><a href="#"><p>Item two</p></a></li>
-					<li id="button-three"><a href="#"><p>Item three</p></a></li>
+					<li id="button-one"><a href="#"><p>VOLUNTEER</p></a></li>
+					<li id="button-two"><a href="#"><p>OUR MISSION</p></a></li>
+					<li id="button-three"><a href="#"><p>DONATE</p></a></li>
 				</ul>
 			</div>
 
-		<div id="button-container2">
-			<ul id="button-list2">
-				<li id="button-four"><a href="#"><p>Item four</p></a></li>
-				<li id="button-five"><a href="#"><p>Item five</p></a></li>
-				<li id="button-six">
-					<div id="social-networking">
-						<div id="find-us"><p><strong>Find Us:</strong></p></div>
-						<div id="facebook"></div>
-						<div id="twitter"></div>
-					</div>
-				</li>
+			<div id="button-container2">
+				<ul id="button-list2">
+					<li id="button-four"><a href="#"><p>NEED HELP?</p></a></li>
+					<li id="button-five"><a href="#"><p>SIGN UP</p></a></li>
+					<li id="button-six">
+						<div id="social-networking">
+							<div id="find-us"><p><strong>Find Us:</strong></p></div>
+							<div id="facebook"></div>
+							<div id="twitter"></div>
+						</div>
+					</li>
 
-			</ul>
-		</div>
+				</ul>
+			</div>
 			<div id="testimonials">
-				Testimonials
+				<span>Testimonials</span>
+              <img src="images/testimonials.png" alt="testimonials">
+                    <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi. Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi.</p>
+                    <a href="">Read More  >></a> 
 			</div>
 			<div id="ads">
 				Ads
