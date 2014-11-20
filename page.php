02c7ee9d21240cc02e20fde2ab7342ae648b8320
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-19 23:15:19 -0800

    changes of divs for page.php and its corresponding styles

--- page.php
+++ page.php
@@ -1,13 +1,14 @@
 <?php get_header(); ?>
-
-<div class="main_content left">
-	<?php if(have_posts()): while(have_posts()): the_post(); ?>
-		<h2><?php the_title(); ?></h2>
-		<?php the_post_thumbnail(); ?>
-		<?php the_content();?>
-	<?php endwhile; else: ?>
-		<p>Sorry, we couldn't find your page!</p>
-	<?php endif; ?>
+<div id="main-content">
+	<div id="pagecontent">
+		<?php if(have_posts()): while(have_posts()): the_post(); ?>
+			<h2><?php the_title(); ?></h2>
+			<?php the_post_thumbnail(); ?>
+			<?php the_content();?>
+		<?php endwhile; else: ?>
+			<p>Sorry, we couldn't find your page!</p>
+		<?php endif; ?>
+	</div>
 </div>
 
 <?php get_sidebar("right"); ?>
