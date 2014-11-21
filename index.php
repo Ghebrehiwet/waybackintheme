13ea13668a4dbe598f9b550ea52a7688c7fc2a23
Ghebrehiwet <geberhane@gmail.com>
2014-11-20 19:00:01 -0800

    changes to the index

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php get_header(); ?>
 <div id="pagecontent"><!--open #content -->
 	<?php if(have_posts()): while(have_posts()): the_post(); ?>
-		<h2><?php the_title(); ?></h2>
-	<!-- <?php get_search_form();?> -->
-	<!--<?php get_sidebar("right"); ?> -->
+		<h2><?php the_title(); ?></h2> 
 	<!--<article class="halfcol left">-->	
 		
 		<div id="post"><!--open #post -->
