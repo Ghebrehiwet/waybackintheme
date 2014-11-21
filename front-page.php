f51c732e0b7fab67ce49573a10a5308fa440af7d
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-21 11:22:50 -0800

    work again on the testimonial section

--- front-page.php
+++ front-page.php
@@ -46,9 +46,8 @@ Template Name: Front Page
                     <p> 
 					
 					<?php if(have_posts()): while(have_posts()): the_post(); ?>
-		<h2><?php the_title(); ?></h2> 
-	<!--<article class="halfcol left">-->	
-		
+		 
+	 
 		<div id="post"><!--open #post -->
 						 
 				<div class="post">
