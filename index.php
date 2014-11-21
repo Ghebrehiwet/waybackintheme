f35712ace8a0414152152b7fdd6ca914fe456e75
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-21 13:34:50 -0800

    adds the thumbnail tag back

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
 						by <?php the_author();?>,
 						<?php comments_number("0 comments", "1 comment", "% comments"); ?>
 					 
-					<div class="post-list"> 
+					<div class="post-list">
+						<?php the_post_thumbnail(); ?>
 						<p><?php the_content(); ?></p>
 					</div>
 				</div>
