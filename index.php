fe4a4e9115f55160d0bff2b138b1ebd042ee53eb
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-21 13:12:21 -0800

    multiple styles targeting contents of posts

--- index.php
+++ index.php
@@ -16,8 +16,10 @@
 						<?php comments_number("0 comments", "1 comment", "% comments"); ?>
 					 
 					<div class="post-list">
-						<?php the_post_thumbnail(); ?>
-						<p><?php the_content(); ?></p>
+						<span>
+							<?php the_post_thumbnail(); ?>
+							<p><?php the_content(); ?></p>
+						<span>
 					</div>
 				</div>
 		</div>
