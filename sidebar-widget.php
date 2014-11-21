440844998bd5bfb15b90a8700bf7564bcfc65644
cluetrain <greenstrat67@gmail.com>
2014-11-20 20:51:10 -0800

    revising sidebar-widget.php

--- sidebar-widget.php
+++ sidebar-widget.php
@@ -1,19 +1,7 @@
 <div id="primarypagesidebar">
-				 <div id="button-container3">
-					<ul id="button-list2">
-						<li id="button-one"><a href="#"><p>Volunteer</p></a></li>
-						<li id="button-two"><a href="#"><p>Donate</p></a></li>
-						<li id="button-three"><a href="#"><p>Sign Up</p></a></li>
-						<li id="button-four"><a href="#"><p>Need Help?</p></a></li>
-						<li id="button-five"><a href="#"><p>Our Mission</p></a></li>
-						<li id="button-six">
-							<div id="social-networking">
-								<div id="find-us"><p><strong>Find Us:</strong></p></div>
-								<div id="facebook"></div>
-								<div id="twitter"></div>
-							</div>
-						</li>
-					</ul>
-				</div> 
-			</div>
+		<div id="widgets">
+			
+		</div>
+				  
+</div>
 
