41a749092a410d09e8b206193be8f168a204c092
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-22 00:08:14 -0800

    more changes to single.php

--- single.php
+++ single.php
@@ -5,19 +5,22 @@
 		<div id="post"><!--open #post --> 
 				<h2><?php the_title(); ?></h2>
 				<?php the_post_thumbnail(); ?>
-				<?php the_content(); ?>
 				<p>Listed in: <?php the_category(", "); ?></p>
+				<small>Posted by <?php the_author();?></small>
+				<time datetime="<?php the_time('Y-m-d'); ?>'><?php the_time('M j'); ?></time>
+				
+				<?php the_content(); ?>
+				
 				<?php if(get_the_tags()): ?>
 					<p><?php the_tags(); ?></p>
 				<?php endif; ?>
 				
 				<h3>Comments</h3>
-			<small>Posted by <?php the_author();?>
-				<time datetime="<?php the_time('Y-m-d'); ?>'><?php the_time('M j'); ?></time>
+			
 				 
 				 
 				<?php comments_number("0 comments", "1 comment", "% comments"); ?>
-			</small>
+			
 			<?php the_author_posts_link(); ?>
 			<?php comments_template(); ?>
 			<ul class="post_nav">
