d0531cde8ea199d4509e4c61f622431d67cc3266
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-21 20:36:20 -0800

    changes of styling to p on the page.php

--- page.php
+++ page.php
@@ -1,10 +1,13 @@
 <?php get_header(); ?>
 <div id="main-content">
-	<div id="pagecontent">
+	
 		<?php if(have_posts()): while(have_posts()): the_post(); ?>
+		
+		<div id="pagecontent">
+		
 			<h2><?php the_title(); ?></h2>
 			<?php the_post_thumbnail(); ?>
-			<?php the_content();?>
+			<p><?php the_content();?><p>
 		<?php endwhile; else: ?>
 			<p>Sorry, we couldn't find your page!</p>
 		<?php endif; ?>
