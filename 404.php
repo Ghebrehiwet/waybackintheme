45506ae2de3393ff0a5c75f4a7e70a9a4c701b8a
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-22 23:39:04 -0800

    makes changes to 404.php

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="main_content left">
-	<p>Sorry. page not found!</p>
+	<p>Sorry for not being able to get of what you have been looking for. Can you kindly search using our search engine below</p>
 	<p><?php get_search_form();?></p>
 </div>
 <?php get_sidebar("right"); ?>
