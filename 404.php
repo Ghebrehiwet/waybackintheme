ae856cb3366cdc9c77aa6d88f6b407cfe24d3651
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-22 23:46:53 -0800

    changes the sidebar for the 404.php

--- 404.php
+++ 404.php
@@ -4,5 +4,5 @@
 	<p>Sorry for not being able to get of what you have been looking for. Can you kindly search using our search engine below</p>
 	<p><?php get_search_form();?></p>
 </div>
-<?php get_sidebar("right"); ?>
+<?php get_sidebar("widget"); ?>
 <?php get_footer();?>
