4cf915da80f7d7cd0a1660bb39a8a77ad47dfb09
Ghebre Ghebremedhin <geberhane@gmail.com>
2014-11-29 17:08:22 -0800

    index.php

--- index.php
+++ index.php
@@ -9,33 +9,44 @@
 		}?>
 	</p>
 
-	<?php if(have_posts()): while(have_posts()): the_post(); ?> 
+	<?php if (have_posts()) : ?>
 			<div id="post"><!--open #post -->			
 				<div class="post">
-					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>	
-					 <p><small>
-						<time datetime="<?php the_time('Y-m-d'); ?>">
-							<?php the_time('M j, Y'); ?>
-						</time>
-						by <?php the_author();?>,
-						<?php comments_number("0 comments", "1 comment", "% comments"); ?>
-					 </small></p>
-					<div class="post-list">
-						
-						<div class="image-left"><?php the_post_thumbnail(); ?></div>
-						<div class="text-right"><?php the_content(); ?></div>
-					</div>
+					<?php while (have_posts()) : the_post(); ?>
+						<div class="post-box">
+							<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2> 
+								 	
+								 <p>Posted on <small>
+									<time datetime="<?php the_time('Y-m-d'); ?>">
+										<?php the_time('M j, Y'); ?>
+									</time> in 
+									<p class="cat"><?php the_category(', ') ?></p>
+									by <?php the_author();?> with
+									<?php comments_number("0 comments", "1 comment", "% comments"); ?>
+								 </small></p>
+								<div class="post-list"> 
+									<div class="image-left"><?php the_post_thumbnail(); ?></div>
+									<div class="text-right"><?php the_content(); ?></div>
+									 <?php the_content('More &raquo;'); ?>
+									<?php edit_post_link('Edit this entry.', '<p><small>', '</small></p>'); ?>
+								</div>
+							
+						</div>
 					</hr>
-				</div>
-			
 				
-			</div>
-		 <hr>
+				</div>
+		  
 	
-	<?php endwhile; else: ?>
-		<p>Sorry no posts to display</p>
-	<?php endif; ?>
-
+						<?php endwhile; ?> 
+							<nav class="post-nav">
+								<p class="alignleft"><?php next_posts_link('&laquo; Older Posts') ?></p>
+								<p class="alignright"><?php previous_posts_link('Newer Posts &raquo;') ?></p>
+							</nav>
+						<?php comments_template(); ?>
+		 </div>
+				
+		<?php endif; ?>
+	
 </div><!--close #pagecontent -->
 <?php get_sidebar("widget"); ?>
 <?php get_footer(); ?>
