aced1fd896ec33293682594bf8f4571b6b5db846
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-30 22:04:48 -0800

    index.php

--- index.php
+++ index.php
@@ -1,49 +1,47 @@
-<?php get_header(); ?>
+<?php get_header(); ?> 
 
-<div id="pagecontent"><!--open #content -->
-	<?php if (have_posts()) : ?>
-		<div id="post"><!--open #post -->
-			<?php while (have_posts()) : the_post(); ?>
-			
-			<p id="bread-crumb">
-					<?php if(function_exists('bcn_display'))
-						{
-							bcn_display();
-						}
-					?>
-				</p>
-			
-				<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2> 
-
-				
-				
-				
-				<p>Listed in: <?php the_category(", "); ?><small> Posted by <?php the_author_posts_link();?></small></p>
-				<time datetime="<?php the_time('Y-m-d'); ?>'><?php the_time('M j'); ?></time>
-				<?php the_post_thumbnail(); ?> 
-				
-				<?php the_content(); ?>
-				
-				<?php if(get_the_tags()): ?>
-					<p><?php the_tags(); ?></p>
-				<?php endif; ?>
-				
-				<h3>Comments</h3> 
+<div id="pagecontent"><!--opening #middle -->
 				 
-				<?php comments_number("0 comments", "1 comment", "% comments"); ?>
-			
-			<?php the_author_posts_link(); ?>
-			<?php comments_template(); ?>
-			<ul class="post_nav">
-				<?php previous_post_link("<li>%link</li>", "&lt; Previous"); ?>
-				<?php next_post_link("<li>%link</li>", "Next &gt;"); ?>
-			</ul>
-		</div>
-			
-			<?php endwhile; else: ?>
-				<p>Sorry we couldn't find your post</p>
-			<?php endif; ?>
-</div><!--close #pagecontent -->
-<?php get_sidebar("widget"); ?>
-<?php get_footer(); ?>
+	<div id="post"><!--open #main --> 
+				<?php if (have_posts()) : ?>
+                <div id="content" class="index">
+                    <?php while (have_posts()) : the_post(); ?>
+                        <div class="post-box">
+                        <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                        <p class="postmetadata">
+                            <span class="date">Posted on <?php the_time('F jS, Y') ?></span> in 
+                            <span class="cat"><?php the_category(', ') ?></span>
+                            <span class="author"><?php _e('By');?> <?php the_author_posts_link(); ?></span>
+                            <span class="comments">with <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></span>
+                        </p>
+						<?php 
+							if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+								the_post_thumbnail();
+							} 
+						?>
+<?php the_content(); ?>
+                        <?php the_content('More &raquo;'); ?>
+                        <?php edit_post_link('Edit this entry.', '<p><small>', '</small></p>'); ?>
+                        </div>
+                    <?php endwhile; ?>
+                
+                        <nav class="post-nav">
+                            <p class="alignleft"><?php next_posts_link('&laquo; Older Posts') ?></p>
+                            <p class="alignright"><?php previous_posts_link('Newer Posts &raquo;') ?></p>
+                        </nav>
+						<?php comments_template(); ?>
+                
+                </div>
+				
+                <?php endif; ?>
+				
+		<?php get_sidebar("widget"); ?>
+ 
 
+		</div>
+								
+	 </div>
+
+
+<?php get_footer(); ?>
+<?php wp_link_pages(); ?>
