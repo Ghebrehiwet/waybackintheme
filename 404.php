df904e3f6e145ffd71fb7d1165e9d63f9f97618a
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-30 23:48:07 -0800

    404.php

--- 404.php
+++ 404.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
-<div class="main_content left">
-	<p>Sorry for not being able to get of what you have been looking for. Can you kindly search using our search engine below</p>
-	<p><?php get_search_form();?></p>
+<div id="pagecontent"><!--opening #middle -->
+	<div id="content" class="404">
+		<p>Sorry for not being able to get of what you have been looking for. Can you kindly search using our search engine below</p>
+		<p><?php get_search_form();?></p>
+	</div>
 </div>
 <?php get_sidebar("widget"); ?>
 <?php get_footer();?>
