69591148dc4d65c5517ac9b3d08fc5bde29ffb5b
geberhane@gmail.com <geberhane@gmail.com>
2014-12-02 19:12:36 -0800

    404.php

--- 404.php
+++ 404.php
@@ -8,6 +8,7 @@
 			}?>
 			</p>
 	<div id="content" class="404">
+		<h3>Content Not found!</h3>
 		<p>Sorry for not being able to get of what you have been looking for. Can you kindly search using our search engine below</p>
 		<p><?php get_search_form();?></p>
 	</div>
