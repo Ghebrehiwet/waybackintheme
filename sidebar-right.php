e8be97707167c01c360010b83595fc4128550981
geberhane@gmail.com <geberhane@gmail.com>
2014-12-02 20:02:41 -0800

    restructuring the sidebar-right.php

--- sidebar-right.php
+++ sidebar-right.php
@@ -1,12 +1,16 @@
 <div id="primarypagesidebar">
-				 <div id="button-container3">
-					<ul id="button-list2">
-						<li id="button-one"><a href="#"><p>Volunteer</p></a></li>
-						<li id="button-two"><a href="#"><p>Donate</p></a></li>
-						<li id="button-three"><a href="#"><p>Sign Up</p></a></li>
-						<li id="button-four"><a href="#"><p>Need Help?</p></a></li>
-						<li id="button-five"><a href="#"><p>Our Mission</p></a></li>
-						<li id="button-six">
+				 <div id="side-bar-buttons1">
+					<ul id="button-list3">
+						<li id="side-button-one"><a href="#"><p>Volunteer</p></a></li>
+						<li id="side-button-two"><a href="#"><p>Donate</p></a></li>
+						<li id="side-button-three"><a href="#"><p>Sign Up</p></a></li>
+					</ul>
+				</div>
+				<div id="side-bar-buttons2">
+					<ul id="button-list4">
+						<li id="side-button-four"><a href="#"><p>Need Help?</p></a></li>
+						<li id="side-button-five"><a href="#"><p>Our Mission</p></a></li>
+						<li id="side-button-six">
 							<div id="social-networking"> 
 								<div id="facebook"><img src="<?php bloginfo('template_url'); ?>/images/logo-facebook.jpg" alt="facebook"></a></div>
 							<div id="twitter"><img src="<?php bloginfo('template_url'); ?>/images/twitter-logo2.jpg" alt="twitter"></a></div>
@@ -14,5 +18,5 @@
 						</li>
 					</ul>
 				</div> 
-			</div>
+</div>
 
