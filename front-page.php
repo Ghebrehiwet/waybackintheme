1332c6dc16e0a09789efbb89b7d8d00022b9a4ba
ghebre ghebremedhin <geberhane@gmail.com>
2014-12-02 23:45:50 -0800

    front-page.php

--- front-page.php
+++ front-page.php
@@ -50,7 +50,7 @@ Template Name: Front Page
               <img src="<?php bloginfo('template_url'); ?>/images/testimonials.png" alt="testimonials">
                     <?php
 						if ( is_home() ) {
-								query_posts( 'cat=testimonials' );
+								query_posts( 'cat=Testimonials' );
 							} 
 					?>
 					<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi. Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi.</p>
