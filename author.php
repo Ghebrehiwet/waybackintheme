5372ade4b7f1ca95b29221402f70b87da5dd7e3e
geberhane@gmail.com <geberhane@gmail.com>
2014-12-02 19:18:55 -0800

    author.php

--- author.php
+++ author.php
@@ -2,47 +2,80 @@
 
 	<div id="pagecontent"><!--open #content -->
 
-			<?php if ( have_posts() ) : ?>
-
-			 
-				 
-					<?php 
-						printf( __( 'All posts by %s', 'twentyfourteen' ), get_the_author() );
-					?>
-				 
-				<?php if ( get_the_author_meta( 'description' ) ) : ?>
-				<div class="author-description"><?php the_author_meta( 'description' ); ?></div>
-				<?php endif; ?>
-			 
-
-			<?php
-					/*
-					 * Since we called the_post() above, we need to rewind
-					 * the loop back to the beginning that way we can run
-					 * the loop properly, in full.
-					 */
-					rewind_posts();
-
-					// Start the Loop.
-					while ( have_posts() ) : the_post();
-
-						/*
-						 * Include the post format-specific template for the content. If you want to
-						 * use this in a child theme, then include a file called called content-___.php
-						 * (where ___ is the post format) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-
-					endwhile;
-					// Previous/next page navigation.
-					twentyfourteen_paging_nav();
-
+			<div class="breadcrumbs">
+					<?php if(function_exists('bcn_display'))
+					{
+						bcn_display();
+					}?>
+				</div>
+		<div id="content" class="author">
+			<!-- This sets the $curauth variable -->
+				<?php
+				if(isset($_GET['author_name'])) :
+					$curauth = get_userdatabylogin($author_name);
 				else :
-					// If no content, include the "No posts found" template.
-					get_template_part( 'content', 'none' );
-
+					$curauth = get_userdata(intval($author));
 				endif;
+				?>
+				<h2 class="posttitle"><?php echo $curauth->display_name; ?></h2>
+				<p>
+			<?php if(!empty($curauth->userphoto_thumb_file)){
 			?>
+				   <div style="float:left;padding-right:15px;padding-bottom:15px;"><img class="photo" alt="<?php echo $curauth->display_name; ?>" src="/wp-content/uploads/userphoto/<?php echo $curauth->userphoto_thumb_file; ?>" /></div>
+			<?php
+			}
+			?>
+
+			<?php 
+				if(empty($curauth->user_description))	echo $curauth->first_name. ' Here are what you have authored.';
+				else echo $curauth->user_description; 
+				?></p>
+				<?php
+				if(!empty($curauth->user_url)){
+					?>
+					<p>Website: <a href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a></p>
+					<?
+				}
+				if(!empty($curauth->twitter)){
+					?>
+					<p>Twitter: <a href="http://twitter.com/<?php echo $curauth->twitter; ?>">http://twitter.com/<?php echo $curauth->twitter; ?></a></p>
+					<?
+				}
+				?>
+				
+				<h2 class="posttitle">Posts by <?php echo $curauth->first_name; ?></h2>
+
+				<ul>
+			<!-- The Loop -->
+
+				<?php 
+					$hasposts = have_posts();
+					if ( $hasposts ) : while ( have_posts() ) : the_post(); ?>
+					<li>
+						<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>">
+						<?php the_title(); ?></a>,
+						<?php the_time('d M Y'); ?> in <?php the_category(', ');?>
+					</li>
+				
+					<?php endwhile; ?>
+				</ul>	
+					<div class="post-nav">
+						<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+						<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+					</div>
+
+				<?php else : ?>
+					<p><?php _e( $curauth->first_name.' has not written anything yet.'); ?></p>
+
+				<?php endif; ?>
+
+				<?php if($hasposts) echo ''; ?>
+
+			<!-- End Loop -->
+					
+			<!-- END CONTENT -->
+
+		</div>
 
 		</div><!-- #content -->
 	 
