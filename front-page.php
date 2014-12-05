a25590520825a24e1374cb81a103bae0b14adb33
Ghebre Ghebremedhin <geberhane@gmail.com>
2014-12-04 19:43:50 -0800

    Still stuff not to do

--- front-page.php
+++ front-page.php
@@ -52,11 +52,10 @@ Template Name: Front Page
 						$testimonials = new WP_Query( array( "category_name" => "testimonials" ));
 						$testimonials->the_post();
 					?>
-					<h3><?php $testimonials->the_title(); ?></h3>
-					<p><?php $testimonials->the_excerpt(); ?></p>
-					?>
-					<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi. Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi.</p>
-                    <a href="">Read More  >></a> 
+					<h3><?php the_title(); ?></h3>
+					<p><?php the_excerpt(); ?></p>
+					<a href="">Read More  &rAquo;</a> 
+					<?php wp_reset_postdata(); ?>
 			</div>
 			<div id="ads">
 					<div id="amazon-smile">
