b9b6a1bbe49b7b9e857d4790bfc2ad1b73bd4479
Ghebre Ghebremedhin <geberhane@gmail.com>
2014-12-04 19:32:12 -0800

    Use WP_Query

--- front-page.php
+++ front-page.php
@@ -49,11 +49,9 @@ Template Name: Front Page
 					<?php the_content(); ?>
               <img src="<?php bloginfo('template_url'); ?>/images/testimonials.png" alt="testimonials">
                     <?php
-						$args = array(
-							'cat'      => Testimonials,
-							 
-						);
-						query_posts( $args );
+						$testimonials = new WP_Query( array( "category_name" => "testimonials" ));
+						$testimonials->the_post();
+						$testimonials->the_title();
 					?>
 					<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi. Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi.</p>
                     <a href="">Read More  >></a> 
