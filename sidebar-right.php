f772b0a7a00a27565b56e432195a71696e4db0ac
Belvatron <r4gn4rkv3rn@gmail.com>
2014-12-08 16:22:58 -0800

    linking buttons

--- sidebar-right.php
+++ sidebar-right.php
@@ -1,15 +1,15 @@
 <div id="primarypagesidebar">
 				 <div id="side-bar-buttons1">
 					<ul id="button-list3">
-						<li id="side-button-one"><a href="#"><p>Volunteer</p></a></li>
-						<li id="side-button-two"><a href="#"><p>Donate</p></a></li>
+						<li id="side-button-one"><a href="http://ec2-54-69-107-154.us-west-2.compute.amazonaws.com/get-involved/volunteer-application/"><p>Volunteer</p></a></li>
+						<li id="side-button-two"><a href="https://www.justgive.org/basket?acton=donate&ein=91-1548418"><p>Donate</p></a></li>
 						<li id="side-button-three"><a href="#"><p>Sign Up</p></a></li>
 					</ul>
 				</div>
 				<div id="side-bar-buttons2">
 					<ul id="button-list4">
 						<li id="side-button-four"><a href="#"><p>Need Help?</p></a></li>
-						<li id="side-button-five"><a href="#"><p>Our Mission</p></a></li>
+						<li id="side-button-five"><a href="http://ec2-54-69-107-154.us-west-2.compute.amazonaws.com/about-us/our-history-how-we-help/"><p>Our Mission</p></a></li>
 						<li id="side-button-six">
 							<div id="sidebar-social-networking">
 								<div id="sidebar-facebook"><img src="<?php bloginfo('template_url'); ?>/images/logo-facebook.jpg" alt="facebook"></a></div>
